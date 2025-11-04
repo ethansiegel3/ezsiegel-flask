09efbbf429e5b9a57eacaa713145283459816540
ethansiegel3 <ezsiegel@uiowa.edu>
2025-11-03 19:29:28 -0600

    added all secondary applications

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # Module 10 -- Flask 
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return "About Flask!"
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
